7e4a0379cbde6bbe4f1e3a9ac04e818654fb6e6c
JSENL <jasenlambright@gmail.com>
2018-05-07 20:27:54 -0400

    Commit message

---
+++ assets/js/todo.js
@@ -0,0 +1,19 @@
+// “Check off specific tasks by clicking on them”
+$('li').click(function(){
+	$(this).css('color', 'gray');
+	$(this).css('textDecoration', 'line-through');
+	if ($(this).css('color') === 'rgb(128, 128, 128)'){
+		console.log('It is currently gray');
+			}
+			else({
+				$(this).css('color') === 'rgb(128, 128, 128)'
+			}
+
+				)
+		})
+
+// if <li> is already gray
+	// turn it black
+	// else 
+	// turn it gray
+
